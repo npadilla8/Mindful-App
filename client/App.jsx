7ad0af3549be9a6671523ec8f5977273ab12d8af
aygulpardane <aygulparpucu@gmail.com>
2023-11-08 15:37:28 -0700

    add HomePage route to App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
 
     return (
-      <div className="App">
-        Hello world!
-      </div>
+      <Router>
+        <Route path="/" element={<HomePage />} />
+      </Router>
     );
   }
-  
+
   export default App;
