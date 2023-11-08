857c9ec7129851a9e758e3d05bd4fa6b858d00b9
Stacey Sanchez <staceysanchez27@gmail.com>
2023-11-08 14:05:49 -0500

    images

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main(){
+    //seeding category table
     await prisma.category.upsert({
         where: { name: 'Clothing & Jewelry' },
         update: {},
@@ -28,6 +29,95 @@ async function main(){
         update: {},
         create: {
             name: 'Home & Living'
+        }
+    });
+
+    //seeding product table 
+    //collectibles & art
+    await prisma.product.upsert({
+        where: { title: 'Corner Bookmark' },
+        update: {},
+        create: {
+            title: 'Corner Bookmark',
+            image: './product_images/collectibles & art/Corner Bookmark.avif',
+            description: 'Personalized Hand Embroidered Corner Bookmark, Book Lovers',
+            price: 9,
+            available: true,
+            returnPolicy: true,
+            quantity: 5,
+            categoryId: 3
+        }
+    });
+    await prisma.product.upsert({
+        where: { title: 'Acrylic Bookmark' },
+        update: {},
+        create: {
+            title: 'Acrylic Bookmark',
+            image: './product_images/collectibles & art/Acrylic Bookmark.avif',
+            description: 'Personalized Name Acrylic Bookmark, Handmade Floral Bookmark',
+            price: 11,
+            available: true,
+            returnPolicy: true,
+            quantity: 9,
+            categoryId: 3
+        }
+    });
+    await prisma.product.upsert({
+        where: { title: 'Watercolor Painting' },
+        update: {},
+        create: {
+            title: 'Watercolor Painting',
+            image: './product_images/collectibles & art/Watercolor Painting.webp',
+            description: 'Dog Painting Custom From Photo, Pet Portrait Print ',
+            price: 80,
+            available: true,
+            returnPolicy: false,
+            quantity: 1,
+            categoryId: 3
+        }
+    });
+
+    //clothing & jewelry
+    await prisma.product.upsert({
+        where: { title: 'Stethoscope Necklace' },
+        update: {},
+        create: {
+            title: 'Stethoscope Necklace',
+            image: './product_images/clothing & jewelry/Stethoscope Necklace.avif',
+            description: 'Custom Stethoscope Necklace With Name, Healthcare Worker',
+            price: 60,
+            available: true,
+            returnPolicy: true,
+            quantity: 10,
+            categoryId: 1
+        }
+    });
+    await prisma.product.upsert({
+        where: { title: 'Birthstone Necklace' },
+        update: {},
+        create: {
+            title: 'Birthstone Necklace',
+            image: './product_images/clothing & jewelry/Birthstone Necklace.avif',
+            description: 'Raw Aquamarine Necklace, March Birthstone, Crystal Rough Gemstone',
+            price: 31,
+            available: true,
+            returnPolicy: true,
+            quantity: 8,
+            categoryId: 1
+        }
+    });
+    await prisma.product.upsert({
+        where: { title: 'Personalized Sweater' },
+        update: {},
+        create: {
+            title: 'Personalized Sweater',
+            image: './product_images/clothing & jewelry/Stethoscope Necklace.avif',
+            description: 'Hand Embroidered With Name, Baby and Toddler Chunky Sweater',
+            price: 82,
+            available: true,
+            returnPolicy: false,
+            quantity: 3,
+            categoryId: 1
         }
     });
 }
