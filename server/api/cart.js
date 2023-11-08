25a70753a052b1a888bb16c8a1a65cbe62719311
aygulpardane <aygulparpucu@gmail.com>
2023-11-08 11:27:22 -0700

    add GET query for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,7 +1,27 @@
+const express = require("express");
+const cartRouter = express.Router();
+const prisma = require("../db/index");
 
+// TODO: add {requireUser} function
 
 //GET /api/cart/:cartId
+cartRouter.get("/:cartId", async(req, res, next) => {
+    try {
+        const cart = await prisma.cart.findUniqueOrThrow({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(cart);
+    } catch (error) {
+        next("Unable to get cart");
+    }
+});
 
 //POST /api/cart
 
 //PUT /api/cart
+
+//DELETE /api/cart
+
+module.exports = cartRouter;
