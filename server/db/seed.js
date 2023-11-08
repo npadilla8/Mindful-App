81db85f4b0c98edfcf0be043f338efe084746a07
Stacey Sanchez <staceysanchez27@gmail.com>
2023-11-08 11:33:09 -0500

    seeding category table

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,1 +1,42 @@
-// TODO: seed the DB using Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main(){
+    await prisma.category.upsert({
+        where: { name: 'Clothing & Jewelry' },
+        update: {},
+        create: {
+            name: 'Clothing & Jewelry'
+        }
+    });
+    await prisma.category.upsert({
+        where: { name: 'Toys' },
+        update: {},
+        create: {
+            name: 'Toys'
+        }
+    });
+    await prisma.category.upsert({
+        where: { name: 'Collectibles & Art' },
+        update: {},
+        create: {
+            name: 'Collectibles & Art'
+        }
+    });
+    await prisma.category.upsert({
+        where: { name: 'Home & Living' },
+        update: {},
+        create: {
+            name: 'Home & Living'
+        }
+    });
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (error) => {
+        console.error(error)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
