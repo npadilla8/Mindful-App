e148b1784fb2a2430381883b34adc2f4468aaa11
aygulpardane <aygulparpucu@gmail.com>
2023-11-08 18:51:48 -0700

    Update cart.js requests

--- server/api/cart.js
+++ server/api/cart.js
@@ -23,54 +23,54 @@ cartRouter.get("/:cartId", async (req, res, next) => {
 // creates a new record in the relation table cartItem (items:create)
 // connects the product assignment to existing product (product:connect)
 // connects the cart to the user
-cartRouter.post("/", async (req, res, next) => {
-    try {
-        const quantity = req.body.quantity;
-        const cart = await prisma.cart.create({
-            data: {
-                items: {
-                    create: [
-                        {
-                            quantity,
-                            product: {
-                                connect: {
-                                    id: productId // FIX HERE - productId won't work
-                                }
-                            }
-                        }
-                    ]
-                },
-                user: {
-                    connect: {
-                        id: req.user.id
-                    }
-                }
-            }
-        });
-        res.send(cart);
-    } catch (error) {
-        next("Unable to create cart")
-    }
-});
+// cartRouter.post("/", async (req, res, next) => {
+//     try {
+//         const quantity = req.body.quantity;
+//         const cart = await prisma.cart.create({
+//             data: {
+//                 items: {
+//                     create: [
+//                         {
+//                             quantity,
+//                             product: {
+//                                 connect: {
+//                                     id: productId // FIX HERE - productId won't work
+//                                 }
+//                             }
+//                         }
+//                     ]
+//                 },
+//                 user: {
+//                     connect: {
+//                         id: req.user.id
+//                     }
+//                 }
+//             }
+//         });
+//         res.send(cart);
+//     } catch (error) {
+//         next("Unable to create cart")
+//     }
+// });
 
 // PUT /api/cart
-cartRouter.put("/:cartId", async (req, res, next) => {
-    try {
-        // how to get the product that was clicked from req.body?
-        // const { items } = req.body;
-        const cart = await prisma.cart.update({
-            where: {
-                id: Number(req.params.cartId)
-            },
-            data: {
-                items
-            }
-        });
-        res.send(cart);
-    } catch (error) {
-        next("Unable to update cart")
-    }
-});
+// cartRouter.put("/:cartId", async (req, res, next) => {
+//     try {
+//         // how to get the product that was clicked from req.body?
+//         // const { items } = req.body;
+//         const cart = await prisma.cart.update({
+//             where: {
+//                 id: Number(req.params.cartId)
+//             },
+//             data: {
+//                 items
+//             }
+//         });
+//         res.send(cart);
+//     } catch (error) {
+//         next("Unable to update cart")
+//     }
+// });
 
 // DELETE /api/cart/:cartId
 cartRouter.delete("/:cartId", async (req, res, next) => {
