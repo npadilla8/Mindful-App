f2d15c8c3e1fe6db15b913c331620476f61be216
Nicole Padilla <npadilla8@bloomberg.net>
2023-11-09 19:19:49 -0500

    still working on api for products

--- server/api/products.js
+++ server/api/products.js
@@ -2,7 +2,19 @@
 
 //GET /api/products - get all products
 
+productsRouter.get("/", async (req, res, next) => {
+    try {
+      const products = await prisma.products.findMany({
+        where: { id: req.product.id },
+      });
+      res.send(products);
+    } catch (error) {
+      next(error);
+    }
+  });
+
 //GET /api/products/:productId - get individual product
+
 
 //POST /api/products - add new product
 
