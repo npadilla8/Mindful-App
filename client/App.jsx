a6de7be58460badb1577e79d50580a29144bcb58
aygulpardane <aygulparpucu@gmail.com>
2023-11-09 18:09:40 -0700

    add admin account component

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/LoginPage';
+import AdminAccountPage from './components/AdminAccountPage';
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminAccountPage />} />
       </Routes>
     );
   }
---
+++ client/components/AdminAccountPage.jsx
@@ -0,0 +1,9 @@
+const AdminAccountPage = () => {
+    return (
+        <div>
+            <p>This is the Account Page for the admin user</p>
+        </div>
+    );
+}
+
+export default AdminAccountPage;
