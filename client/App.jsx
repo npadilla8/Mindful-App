3dbef1b0fad9f00d77c868772573eb220af83aae
aygulpardane <aygulparpucu@gmail.com>
2023-11-09 18:32:22 -0700

    Added Cart component

--- client/App.jsx
+++ client/App.jsx
@@ -2,11 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import AdminAccountPage from './components/AdminAccountPage';
-
 import AccountPage from './components/AccountPage';
-
-import Register from './components/Register'
-
+import Register from './components/Register';
+import Cart from './components/Cart';
 
 const App = () => {
 
@@ -15,11 +13,9 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminAccountPage />} />
-
         <Route path="/account" element={<AccountPage />} />
-
         <Route path="/register" element={<Register />} />
-
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     );
   }
