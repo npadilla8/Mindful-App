24acc4e45a4721deedafdeda3df7a93f1e118fd0
aygulpardane <aygulparpucu@gmail.com>
2023-11-09 17:51:56 -0700

    added Account page component

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/LoginPage';
+import AccountPage from './components/AccountPage';
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
       </Routes>
     );
   }
---
+++ client/components/AdminAccountPage.jsx
@@ -0,0 +1,9 @@
+const AdminAccountPage = () => {
+    return (
+        <div>
+            <p>This is the Account Page for a logged-in user</p>
+        </div>
+    );
+}
+
+export default AccountPage;
