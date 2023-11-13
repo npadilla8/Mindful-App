1e20b4fcaddcdc2968cf6b9aba46a7b87d747cba
Stacey Sanchez <staceysanchez27@gmail.com>
2023-11-12 21:09:16 -0500

    fixing errors to view front end while working on redux

--- client/components/API/mindfulHarvestApi.js
+++ client/components/API/mindfulHarvestApi.js
@@ -1,2 +1,25 @@
-import {createApi, fetchBaseQuery} from "reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
+const mindfulHarvestApi = createApi({
+    reducerPath: "mindfulHarvestApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "",
+
+        prepareHeaders: (headers, {getState}) => {
+            headers.set("Content-Type", "application/json");
+            const {token} = getState();
+
+            if(token){
+                headers.set("authorization", `Bearer ${token}`);
+            }
+
+            return headers;
+        },
+    }),
+
+    endpoints: (builder) => ({
+        
+    })
+});
+
+export default mindfulHarvestApi;
