cd7d0d6735b4b6384b2cbf3835c8ad2015aef488
aygulpardane <aygulparpucu@gmail.com>
2023-11-15 15:59:44 -0600

    single product page is rendering

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -4,26 +4,26 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { createNextState } from '@reduxjs/toolkit';
 
 const SingleProduct = () => {
+    const { productId } = useParams();
     const {data, error, isLoading} = useGetSingleProductQuery(productId);
     const navigate = useNavigate();
-    const { productId } = useParams();
 
-    const handleAddToCart = async (e) => {
-        e.preventDefault();
+    // const handleAddToCart = async (e) => {
+    //     e.preventDefault();
 
-        try {
-            // TODO: fill with what happens when add to cart is clicked
-        } catch (error) {
-            console.error(error);
-        };
-        navigate('/cart');
-    };
+    //     try {
+    //         // TODO: fill with what happens when add to cart is clicked
+    //     } catch (error) {
+    //         console.error(error);
+    //     };
+    //     navigate('/cart');
+    // };
 
     console.log(data);
 
     return (
         <div className='single-product'>
-            <div key={data}></div>
+            <div key={data}>Single product page here</div>
         </div>
     );
 };
