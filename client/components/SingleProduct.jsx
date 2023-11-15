731087fd985ee3473ce8e29d75233ec41d7ecd56
aygulpardane <aygulparpucu@gmail.com>
2023-11-15 15:42:28 -0600

    add handleAddToCart

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { useGetSingleProductQuery } from './API/mindfulHarvestApi';
 import { useParams, useNavigate } from 'react-router-dom';
+import { createNextState } from '@reduxjs/toolkit';
 
 const SingleProduct = () => {
+    const {data, error, isLoading} = useGetSingleProductQuery(productId);
+    const navigate = useNavigate();
+    const { productId } = useParams();
+
+    const handleAddToCart = async (e) => {
+        e.preventDefault();
+
+        try {
+            // TODO: fill with what happens when add to cart is clicked
+        } catch (error) {
+            console.error(error);
+        };
+        navigate('/cart');
+    };
+
     return (
         <div>
             <p>This is the single product page</p>
