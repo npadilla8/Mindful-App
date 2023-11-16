84898374059710916c7f68264d3debb0d2c591d7
Mariana <mariferraldo@gmail.com>
2023-11-15 19:53:27 -0500

    adding the nav bar

--- client/App.jsx
+++ client/App.jsx
@@ -5,10 +5,13 @@ import AdminAccountPage from './components/AdminAccount/AdminAccountPage';
 import AccountPage from './components/AccountPage';
 import Register from './components/Register';
 import Cart from './components/Cart';
+import NavBar from './components/Navigation';
 
 const App = () => {
 
     return (
+      <div> 
+      <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -17,6 +20,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+      </div>
     );
   }
   export default App;
