908898c6852ba75f31b849dd3003fdd0a7de583f
Stacey Sanchez <staceysanchez27@gmail.com>
2023-11-21 15:16:18 -0500

    format

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -8,18 +8,18 @@ const CartItem = (props) => {
 
     const [quantity, setQuantity] = useState(item.quantity)
     const [updateQuantityOfCartItem] = useUpdateQuantityOfCartItemMutation();
-    
+
     const { data: singleProductData, error: productError, isLoading: productIsLoading } = useGetSingleProductQuery(item.productId);
-  
+
     if (productIsLoading) {
-      return <div>Loading product...</div>;
-    };
-  
-    if (productError || !singleProductData) {
-      return <div>Error in showing cart item.</div>;
+        return <div>Loading product...</div>;
     };
 
-    async function handleEditItemQuantity(event){
+    if (productError || !singleProductData) {
+        return <div>Error in showing cart item.</div>;
+    };
+
+    async function handleEditItemQuantity(event) {
         event.preventDefault();
 
         const response = await updateQuantityOfCartItem({
@@ -28,22 +28,22 @@ const CartItem = (props) => {
         });
         console.log("cart item quanity change: ", response)
     }
-  
-    return (
-      <div key={item.id}>
-        <p><b>{singleProductData.title}</b></p>
-        <img style={{ width: "40%" }} src={singleProductData.image} alt={singleProductData.title} />
-        <p>Price: ${singleProductData.price}</p>
-        <p>Quantity: {item.quantity}</p>
-        <label>Quantity: {" "} 
-        <input value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-        </label>
-        <button onClick={handleEditItemQuantity}>Edit</button>
-        <br/>
-        <br/>
-        <button onClick={() => handleCartItemRemoval(item.id)}>Remove</button>
-      </div>
-    );
-  };
 
-  export default CartItem
+    return (
+        <div key={item.id}>
+            <p><b>{singleProductData.title}</b></p>
+            <img style={{ width: "40%" }} src={singleProductData.image} alt={singleProductData.title} />
+            <p>Price: ${singleProductData.price}</p>
+            <p>Quantity: {item.quantity}</p>
+            <label>Quantity: {" "}
+                <input value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            </label>
+            <button onClick={handleEditItemQuantity}>Edit</button>
+            <br />
+            <br />
+            <button onClick={() => handleCartItemRemoval(item.id)}>Remove</button>
+        </div>
+    );
+};
+
+export default CartItem
