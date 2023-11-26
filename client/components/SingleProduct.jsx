47966fa3074222e0adff108903cd48dab4279b86
Nicole Padilla <npadilla8@bloomberg.net>
2023-11-26 13:08:14 -0500

    addind more revision to pass

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  useAddCartItemtoCartMutation,
-  useGetSingleProductQuery,
-} from './API/mindfulHarvestApi';
+import { useAddCartItemtoCartMutation, useGetSingleProductQuery } from './API/mindfulHarvestApi';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import AddCircleTwoToneIcon from '@mui/icons-material/AddCircleTwoTone';
@@ -14,120 +11,105 @@ import { setCart } from './API/cartSlice';
 import { Box, Grid, Card, CardContent, Button, Typography } from '@mui/material';
 
 const SingleProduct = () => {
-  const [amount, setAmount] = useState(1);
-  const { productId } = useParams();
-  const { data, error, isLoading } = useGetSingleProductQuery(productId);
-  const navigate = useNavigate();
-  const [addToCart] = useAddCartItemtoCartMutation();
-  const token = useSelector((state) => state.token);
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
+    const [amount, setAmount] = useState(1);
+    const { productId } = useParams();
+    const {data, error, isLoading} = useGetSingleProductQuery(productId);
+    const navigate = useNavigate();
+    const [addToCart] = useAddCartItemtoCartMutation();
+    const token = useSelector(state => state.token);
+    const cart = useSelector(state => state.cart);
+    const dispatch = useDispatch();
 
-  const setDecrease = () => {
-    setAmount(amount - 1);
-  };
+    const setDecrease = () => {
+        setAmount(amount - 1);
+    };
+    const setIncrease = () => {
+        setAmount(amount + 1);
+    };
 
-  const setIncrease = () => {
-    setAmount(amount + 1);
-  };
+    if(isLoading) {
+        return <div>Loading ...</div>
+    };
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+    if(error) {
+        return <div>Unable to Get Product</div>
+    };
+    console.log("cart: ", cart);
+    console.log("cart[0]: ", cart[0] && cart[0].quantity);
+    console.log("cart.quantity: ", cart.quantity);
+    if(!data) {
+        return <p>Unable to view product</p>
+    };
 
-  if (error) {
-    return <div>Unable to Get Product</div>;
-  }
+    const handleAddToCart = async (e) => {
+        e.preventDefault();
 
-  if (!data) {
-    return <p>Unable to view product</p>;
-  }
+        try {
+            if(token) {
+                await addToCart({
+                    productId: Number(productId),
+                    quantity: amount
+                });
+            } else {
+                dispatch(setCart({
+                    productId: Number(productId),
+                    quantity: amount
+                }));
+            }
+        } catch (error) {
+            console.error(error);
+        };
+    };
 
-  const handleAddToCart = async (e) => {
-    e.preventDefault();
-
-    try {
-      if (token) {
-        await addToCart({
-          productId: Number(productId),
-          quantity: amount,
-        });
-      } else {
-        dispatch(
-          setCart({
-            productId: Number(productId),
-            quantity: amount,
-          })
-        );
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <Box sx={{ margin: 5 }}>
-      <Grid container justifyContent="center">
-        <Grid item>
-          <Card sx={{ maxWidth: 1000 }}>
-            <CardContent align="center">
-              <div className="single-product">
-                <div key={data.id}>
-                  <div className="single-product-details">
+    return (
+        <Box sx={{ margin: 5 }}>
+            <Grid container justifyContent="center">
+                <Grid item >
+                    <Card sx={{ maxWidth: 1000 }} >
+        <div className='single-product'>
+            <div key={data.id}>
+                <CardContent align="center">
+                <div className="single-product-details">
                     <h2 className="product-title">{data.title}</h2>
                     <p className="product-description">{data.description}</p>
-                  </div>
-
-                  <div className="image-container">
-                    <img
-                      className="single-image"
-                      src={data.image}
-                      alt={data.title}
-                    />
-                  </div>
-
-                  <div className="product-price">
-                    <Typography variant="body1">$ {data.price}</Typography>
-                  </div>
                 </div>
-              </div>
-            </CardContent>
-            <CardContent align="center">
-              <Stack direction="row" spacing={1}>
-                <IconButton
-                  aria-label="remove-button"
-                  color="secondary"
-                  align="center"
-                  onClick={setDecrease}
-                >
-                  <RemoveCircleTwoToneIcon />
-                </IconButton>
-                <p>{amount}</p>
-                <IconButton
-                  aria-label="add-button"
-                  color="secondary"
-                  align="center"
-                  onClick={setIncrease}
-                >
-                  <AddCircleTwoToneIcon />
-                </IconButton>
-              </Stack>
 
-              <Button
-                className="add-to-cart-button"
-                variant="contained"
+                <div className="image-container">
+                    <img className="single-image" src={data.image} alt={data.title} />
+                </div>
+
+                <div className="product-price">
+                    <Typography variant="body1">$ {data.price}</Typography>
+                </div>
+                     </CardContent>
+                        <CardContent align="center">
+                <Stack direction="row" spacing={1}>
+                    <IconButton aria-label="remove-button" color="secondary" align="center" onClick={setDecrease}>
+                        <RemoveCircleTwoToneIcon />
+                    </IconButton>
+                    <p>{amount}</p>r
+                    <IconButton aria-label="add-button"  color="secondary" align="center" onClick={setIncrease}>
+                        <AddCircleTwoToneIcon />
+                    </IconButton>
+                </Stack>
+
+                <Button 
+                className='add-to-cart-button' 
+                variant="contained" 
                 color="secondary"
-                onClick={handleAddToCart}
-              >
-                Add to Cart
-              </Button>
-            </CardContent>
-          </Card>
+                onClick={handleAddToCart}>Add to Cart
+                </Button>
+                </CardContent>
+            </div>
+        
+        </div>
+            
+        </Card>
         </Grid>
-      </Grid>
-    </Box>
-  );
+        </Grid>
+        </Box>
+       
+    );
 };
 
 export default SingleProduct;
-
