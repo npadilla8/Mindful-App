3fb886d1f80a8d8c361fecc7a694ed83edcc9b8d
Stacey Sanchez <staceysanchez27@gmail.com>
2023-12-07 17:15:53 -0500

    more specific error msgs for registration error

--- server/api/users.js
+++ server/api/users.js
@@ -8,7 +8,7 @@ const SALT_COUNT = 10;
 const jwt = require("jsonwebtoken");
 const { JWT } = process.env
 
-const {requireUser, requireAdmin} = require('./utils');
+const { requireUser, requireAdmin } = require('./utils');
 
 //GET /api/users - get all users without their passwords
 usersRouter.get('/', requireAdmin, async (req, res, next) => {
@@ -50,8 +50,8 @@ usersRouter.get('/cart', requireUser, async (req, res, next) => {
 
         res.send(shopperWithCart)
     } catch {
-        console.error (error)
-        next({message: "unable to get individual user", error})
+        console.error(error)
+        next({ message: "unable to get individual user", error })
     }
 });
 
@@ -71,11 +71,16 @@ usersRouter.post("/register", async (req, res, next) => {
             }
         });
 
-        if (userWithSameUsername || userWithSameEmail) {
+        if (userWithSameUsername) {
             res.status(401)
-            next({ message: "User or email already exists."
+            next({
+                message: "Username already exists."
             })
-        } else {
+        } else if (userWithSameEmail) {
+            res.status(401)
+            next({ message: "Email already exists." })
+        }
+        else {
             const newUser = await prisma.user.create({
                 data: {
                     username: req.body.username,
@@ -91,11 +96,11 @@ usersRouter.post("/register", async (req, res, next) => {
 
             const token = jwt.sign({ id: newUser.id }, JWT);
 
-            res.send({ newUser, token });
+            res.status(200).send({ newUser, token });
         }
     } catch (error) {
         console.error(error);
-        res.send("unable to register")
+        res.send({message: "unable to register"})
     }
 });
 
@@ -112,24 +117,27 @@ usersRouter.post("/login", async (req, res, next) => {
 
         if (!user) {
             res.status(401)
-            next({ message: "Username does not exist."
+            next({
+                message: "Username does not exist."
             })
-        } else{
-
-        const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
-
-        if (!passwordsMatch) {
-         res.status(401)
-         next({
-                message: "Invalid login credentials."
-            });
         } else {
 
-        const token = jwt.sign({ id: user.id }, JWT);
+            const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
 
-        delete (user.hashedPassword);
+            if (!passwordsMatch) {
+                res.status(401)
+                next({
+                    message: "Invalid login credentials."
+                });
+            } else {
 
-        res.send({ user, token })}}
+                const token = jwt.sign({ id: user.id }, JWT);
+
+                delete (user.hashedPassword);
+
+                res.send({ user, token })
+            }
+        }
     } catch (error) {
 
         res.send({
