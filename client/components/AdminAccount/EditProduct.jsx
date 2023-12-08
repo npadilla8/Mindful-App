d2ed1247f1286820c8e70347f2a67d22ed4dbc34
Mariana <mariferraldo@gmail.com>
2023-12-07 21:27:08 -0500

    fixed the poiter on the nav bar

--- client/components/AdminAccount/EditProduct.jsx
+++ client/components/AdminAccount/EditProduct.jsx
@@ -1,59 +1,64 @@
-import { React } from "react";
+import React from "react";
 import { useGetSingleProductQuery } from "../API/mindfulHarvestApi";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
+import { Typography, CircularProgress, Paper } from "@mui/material";
 import ProductForm from "./ProductForm";
 
 export default function EditProduct() {
-    const adminBoolean = useSelector(state => state.adminBoolean)
-    console.log("admin boolean", adminBoolean)
+    const adminBoolean = useSelector((state) => state.adminBoolean);
+    console.log("admin boolean", adminBoolean);
 
-    if(adminBoolean === false) {
+    if (adminBoolean === false) {
         return (
-            <p> Need Special Permissions to Access Page. </p>
-        )
-    };
-    
-    //getting id from params to render single product
-    const { productId } = useParams();
-    // const singleProductId = params.productId;
+            <Typography variant="body1">
+                Need Special Permissions to Access Page.
+            </Typography>
+        );
+    }
 
-    //rendering single product 
+    // Getting id from params to render single product
+    const { productId } = useParams();
+
+    // Rendering single product
     const { data, error, isLoading } = useGetSingleProductQuery(productId);
 
-    //handling error and loading for useGetSingleProductQuery
+    // Handling error and loading for useGetSingleProductQuery
     if (isLoading) {
-        return <div>Loading...</div>
-    };
+        return <CircularProgress />;
+    }
     if (error) {
-        return <div>Unable to Get Single Product.</div>
-    };
+        return <Typography variant="body1">Unable to Get Single Product.</Typography>;
+    }
 
     return (
-        <>
-            <h3>Edit Product for Sale</h3>
+        <Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
+            <Typography variant="h5">Edit Product for Sale</Typography>
             {data ? (
                 <div>
                     <div>
-                        <p>Title: {data.title}</p>
-                        <p>Description: {data.description}</p>
-                        <img style={{ width: '40%' }} src={data.image} alt={data.title} />
+                        <Typography variant="body1">Title: {data.title}</Typography>
+                        <Typography variant="body1">Description: {data.description}</Typography>
+                        <img style={{ width: "40%" }} src={data.image} alt={data.title} />
                     </div>
                     <div>
-                        <p>Price: {data.price}</p>
-                        <p>Available: {data.available ? "Available" : "Not Available"}</p>
-                        <p>Return Policy: {data.returnPolicy ? "Returnable" : "Non-Returnable"}</p>
-                        <p>Quantity: {data.quantity}</p>
-                        <p>Category Id: {data.categoryId}</p>
+                        <Typography variant="body1">Price: {data.price}</Typography>
+                        <Typography variant="body1">
+                            Available: {data.available ? "Available" : "Not Available"}
+                        </Typography>
+                        <Typography variant="body1">
+                            Return Policy: {data.returnPolicy ? "Returnable" : "Non-Returnable"}
+                        </Typography>
+                        <Typography variant="body1">Quantity: {data.quantity}</Typography>
+                        <Typography variant="body1">Category Id: {data.categoryId}</Typography>
                     </div>
-
                 </div>
             ) : (
-                <p> Unable to View Product </p>
+                <Typography variant="body1">Unable to View Product</Typography>
             )}
 
-            <h3>Update Product Details</h3>
-            <h5>Instructions: Edit one or more fields as needed. </h5>
+            <Typography variant="h5">Update Product Details</Typography>
+            <Typography variant="h6">Instructions: Edit one or more fields as needed.</Typography>
 
             <ProductForm
                 productId={productId}
@@ -66,6 +71,6 @@ export default function EditProduct() {
                 quantity={data.quantity}
                 categoryId={data.categoryId}
             />
-        </>
-    )
+        </Paper>
+    );
 }
--- client/components/CSS/categoriesMenu.js
+++ client/components/CSS/categoriesMenu.js
@@ -81,32 +81,30 @@ export const blue = {
   
   export const MenuButton = styled(BaseMenuButton)(
     ({ theme }) => `
-    font-family: Arial;
-    font-weight: 500;
-    font-size: 0.875rem;
-    line-height: 1.5;
-    padding: 6px 6px;
-    border-radius: 8px;
-    color: white;
-    transition: all 150ms ease;
-    cursor: pointer;
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
-  
-    &:hover {
-      background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
-      border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
-    }
-  
-    &:active {
-      background: ${theme.palette.mode === 'dark' ? grey[700] : grey[100]};
-    }
-  
-    &:focus-visible {
-      box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
-      outline: none;
-    }
+      font-family: Arial;
+      font-weight: 500;
+      font-size: 0.875rem;
+      line-height: 1.5;
+      padding: 6px 6px;
+      border-radius: 8px;
+      transition: all 150ms ease;
+      cursor: pointer;
+      background: transparent; 
+      border: none; 
+      box-shadow: none; 
+    
+      &:hover {
+        background: transparent; 
+        border-color: transparent; 
+      }
+    
+      &:active {
+        background: transparent; 
+      }
+    
+      &:focus-visible {
+        box-shadow: none; 
+        outline: none; 
+      }
     `,
   );
